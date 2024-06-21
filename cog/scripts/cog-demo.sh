b8a29e3a0bf488b51d90ce92b740a222602fc8b1
PlatformKit <54647524+platform-kit@users.noreply.github.com>
2024-06-21 14:50:05 -0700

    restructuring

--- cog/scripts/cog-demo.sh
+++ cog/scripts/cog-demo.sh
@@ -1,8 +1,9 @@
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
         # ...
-        echo "You're on a Linux."
+        echo "You're on Linux."
 elif [[ "$OSTYPE" == "darwin"* ]]; then
         # Mac OSX
         echo "You're on a MacOS."        
 fi
+cd mars5-tts/cog;
 cog predict  -i 'text="Hey there, this is a test."'  -i 'ref_audio_file="https://files.catbox.moe/be6df3.wav"' -i $'ref_audio_transcript="This is text audio, generated with Mars5."'
